d3023030ca62af40081f5a8dd3825d8b73640bdc
jayantp2003 <jayantparakh2003@gmail.com>
2024-09-30 02:19:06 +0530

    Initial commit

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ const userRoutes = require("./routes/user");
 const adminRoutes = require("./routes/admin");
 const product = require("./routes/product");
 const global = require("./routes/global");
+const cors = require("cors");
+const corsOptions = {
+  origin: 'http://localhost:3000', // replace with your frontend URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+app.use(cors(corsOptions));
 
 const uri = process.env.MongoString;
 
@@ -32,6 +40,6 @@ app.use("/admin", adminRoutes);
 app.use("/prod", product);
 app.use("/global", global);
 
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
+app.listen(3003, () => {
+  console.log("Server started on port 3003");
 });
